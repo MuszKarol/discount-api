b342c4a2f2ebf5e57fa8ebd0a494e4b2f4f93c39
MuszKarol <karol.musz99@gmail.com>
2022-08-01 17:37:23 +0200

    Initial commit

---
+++ src/main/java/pl/MuszKarol/DiscountAPI/DiscountApiApplication.java
@@ -0,0 +1,13 @@
+package pl.MuszKarol.DiscountAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscountApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscountApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/MuszKarol/DiscountAPI/DiscountApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.MuszKarol.DiscountAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscountApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
